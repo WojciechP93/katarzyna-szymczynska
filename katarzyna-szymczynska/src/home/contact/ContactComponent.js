8323604a3c2ea19fbc909a9e3b3876545b4dea6d
WojciechP93 <wojpel100@gmail.com>
2023-01-29 18:59:15 +0100

    Add contact component with email form

--- katarzyna-szymczynska/src/home/contact/ContactComponent.js
+++ katarzyna-szymczynska/src/home/contact/ContactComponent.js
@@ -1,74 +1,138 @@
 import * as React from 'react';
-import "./contact.css"
-import { Container, Row, Col, Stack } from 'react-bootstrap/';
+import "./contact.css";
+import { Button, Stack, Form, InputGroup, FormGroup, Image, Modal } from 'react-bootstrap/';
 import SectionHeaderComponent from '../shared/section_header/SectionHeaderComponent';
+import emailjs from '@emailjs/browser';
+import shape1 from "./images/contact-shape1.png";
+import shape2 from "./images/contact-shape2.png";
 
 function ContactComponent() {
+
+    const [validated, setValidated] = React.useState(false);
+    const [show, setShow] = React.useState(false);
+
+    const form = React.useRef();
+    const handleClose = () => setShow(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(true);
+        }
+        else {
+            event.preventDefault();
+            setShow(true);
+        }
+
+
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_nkd8q7m', 'template_vpdiqlm', "#contact-form", 'kqvYLGrSXkzKPXAZo')
+            .then((result) => {
+                alert("Wiadomość wysłana");
+                setShow(false);
+            }, (error) => {
+                alert("Wystąpił błąd podczas wysyłania wiadomości", error);
+            });
+
+        setValidated(true);
+        // e.target.reset();
+    };
+
     return (
         <Stack className='contact-content'>
-            <SectionHeaderComponent title="contact" />
-            <div >
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dol
-                ore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
+
+            <Modal show={show} onHide={handleClose} backdrop="static">
+                <Modal.Header closeButton>
+                    <Modal.Title>Do you want to send message?</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        No
+                    </Button>
+                    <Button variant="primary" onClick={sendEmail}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <SectionHeaderComponent title="contact me" />
+            <p className='contact-header'>
+                Are you interested in cooperation? <br />
+                Contact me!
+            </p>
+            <div className='shape-div'>
+                <Image fluid src={shape1} className="contact-shape" />
             </div>
+            <p className='contact-info'>
+
+                tel: +48 792 940 810 <br />
+                e-mail: k.szymczynska@yahoo.pl
+            </p>
+            <Form ref={form} className='contact-form' noValidate validated={validated} onSubmit={handleSubmit} id="contact-form">
+                <div className='shape-div'>
+                    <Image fluid src={shape2} className="form-shape" />
+                </div>
+                <Stack direction="horizontal" gap={2}>
+                    <FormGroup controlId="formName" className="mb-2 mailer-textbox">
+                        <InputGroup hasValidation>
+                            <Form.Control
+                                name="user_name"
+                                className='form-textbox'
+                                type="text"
+                                placeholder="Name"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                            />
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup controlId="Formemail" className="mb-2 mailer-textbox">
+                        <InputGroup hasValidation>
+                            <Form.Control
+                                className='form-textbox'
+                                type="email"
+                                name="user_email"
+                                placeholder="e-mail"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                            />
+                        </InputGroup>
+                    </FormGroup>
+                </Stack>
+                <FormGroup controlId="formSubject" className="mb-2">
+                    <InputGroup hasValidation>
+                        <Form.Control
+                            className='form-textbox'
+                            name="subject"
+                            type="text"
+                            placeholder="Topic"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup className="mb-2" controlId='formMessage'>
+                    <InputGroup hasValidation>
+                        <Form.Control
+                            name="message"
+                            className='message-textbox form-textbox'
+                            as="textarea"
+                            placeholder="Message"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                    </InputGroup>
+                </FormGroup>
+                <Stack className='button-stack'>
+                    <Button type="submit" className='send-button'>
+                        SEND
+                    </Button>
+                </Stack>
+            </Form>
         </Stack>
     );
 };
