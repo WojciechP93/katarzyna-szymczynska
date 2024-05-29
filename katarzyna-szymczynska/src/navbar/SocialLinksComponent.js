02b0bd97c97cdcdb5d76148a75a776092c595c91
WojciechP93 <wojpel100@gmail.com>
2024-05-29 15:52:43 +0200

    change a to Link

--- katarzyna-szymczynska/src/navbar/SocialLinksComponent.js
+++ katarzyna-szymczynska/src/navbar/SocialLinksComponent.js
@@ -7,17 +7,17 @@ function SocialLinksComponent() {
   return (
     <Nav>
       <Stack direction="horizontal" className="social-links">
-        <a
+        <Link
           href="https://www.linkedin.com/in/katarzyna-szymczy%C5%84ska-72433a98/"
           className="social-link linkedin"
           onclick='window.open("https://www.linkedin.com/in/katarzyna-szymczy%C5%84ska-72433a98/");return false;'
         />
-        <a
+        <Link
           className="social-link instagram"
           onclick='window.open("https://www.instagram.com/katie.design.art?igsh=MXV6aDFrNGVlNHBjdA==");return false;'
           href="https://www.instagram.com/katie.design.art?igsh=MXV6aDFrNGVlNHBjdA=="
         />
-        <a
+        <Link
           className="social-link behance"
           href="https://www.behance.net/KatarzynaSzymczynska"
           onclick='window.open("https://www.behance.net/KatarzynaSzymczynska");return false;'
